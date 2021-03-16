71e5c01fdaeec40f7b4e5358439447400858e344
JabaXNT <gluhow2015@gmail.com>
2021-03-16 16:37:19 +0500

    commit 2

--- Flaskk.py
+++ Flaskk.py
@@ -26,6 +26,28 @@ def mars():
                     <h4>{text2}<h4>
                   </body>
                 </html>"""
+  
+@app.route('/promotion')
+def prom():
+    ads = ['Человечество вырастает из детства.', 'Человечеству мала одна планета.', 'Мы сделаем обитаемыми безжизненные пока планеты.', 'И начнем с Марса!', 'Присоединяйся!']
+    return '</br>'.join(ads)
 
+@app.route('/promotion_image')
+def mars():
+  text = 0
+  text2 = 0
+  return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <title>Привет, Марс!</title>
+                  </head>
+                  <body>
+                    <h1>{text}</h1>
+                    <img src="{url_for('static', filename='img/mars_image.jpg')}" 
+                    alt="здесь должна была быть картинка, но она на Марсе">
+                    <h4>{text2}<h4>
+                  </body>
+                </html>"""
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
