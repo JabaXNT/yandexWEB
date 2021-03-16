11c20787ac58902f7ed122ca9bd1d6c990279a73
JabaXNT <gluhow2015@gmail.com>
2021-03-16 23:03:26 +0500

    Another task

--- yandexFlask.py
+++ yandexFlask.py
@@ -33,21 +33,30 @@ def prom():
     return '</br>'.join(ads)
 
 @app.route('/promotion_image')
-def mars():
-  text = 0
-  text2 = 0
+def mars_prom():
+  text3 = "Жди нас, Марс!"
   return f"""<!doctype html>
                 <html lang="en">
                   <head>
                     <meta charset="utf-8">
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                    <link rel="stylesheet"
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                    crossorigin="anonymous">
                     <title>Привет, Марс!</title>
                   </head>
                   <body>
-                    <h1>{text}</h1>
+                    <h1>{text3}</h1>
                     <img src="{url_for('static', filename='img/mars_image.jpg')}" 
                     alt="здесь должна была быть картинка, но она на Марсе">
-                    <h4>{text2}<h4>
+                    <div class="alert alert-primary" role="alert">Человечество вырастает из детства.</div>
+                    <div class="alert alert-success" role="alert">Человечеству мала одна планета.</div>
+                    <div class="alert alert-secondary" role="alert">Мы сделаем обитаемыми безжизненные пока планеты.</div>
+                    <div class="alert alert-warning" role="alert">И начнем с Марса!</div>
+                    <div class="alert alert-dark alert" role="alert">Присоединяйся!</div>
                   </body>
                 </html>"""
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
