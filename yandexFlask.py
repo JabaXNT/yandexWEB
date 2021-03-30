cbabab82e7cbabe5a27dba767c0b061fdf010da6
JabaXNT <gluhow2015@gmail.com>
2021-03-30 12:45:40 +0500

    dop

--- yandexFlask.py
+++ yandexFlask.py
@@ -132,16 +132,91 @@ def mars():
                 </html>"""
 
 
-@app.route('/odd_even')
-def odd_even():
-    return render_template('odd_even.html', number=2)
+@app.route('/choice/<planet_name>')
+def choice(planet_name):
+  if planet_name == "Плутон":
+        return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                   <link rel="stylesheet"
+                   href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                   integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                   crossorigin="anonymous">
+                    <title>Варианты выбора</title>
+                  </head>
+                  <body>
+                    <h1>Мое предложение: {planet_name}!</h1>
+                    <h2>Неплохая планета<h2>
+                    <div class="alert alert-primary" role="alert">Жаль что беольше не планета</div>
+                    <div class="alert alert-success" role="alert">Но все равно неплохая</div>
+                    <div class="alert alert-secondary" role="alert">Может даже получиться колонизировать</div>
+                    <div class="alert alert-warning" role="alert">бррррррррр</div>
+                    <div class="alert alert-dark alert" role="alert">Присоединяйся!</div>
+                  </body>
+                </html>'''
+  if planet_name == "Земля":
+        return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                   <link rel="stylesheet"
+                   href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                   integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                   crossorigin="anonymous">
+                    <title>Варианты выбора</title>
+                  </head>
+                  <body>
+                    <h1>Мое предложение: {planet_name}!</h1>
+                    <h2>Неплохая планета<h2>
+                    <div class="alert alert-primary" role="alert">Без слов...</div>
+                  </body>
+                </html>'''
+  return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                   <link rel="stylesheet"
+                   href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                   integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                   crossorigin="anonymous">
+                    <title>Варианты выбора</title>
+                  </head>
+                  <body>
+                    <h1>Мое предложение: {planet_name}!</h1>
+                    <h2>Неплохая планета<h2>
+                    <div class="alert alert-primary" role="alert">На ней много необхождимых ресурсов</div>
+                    <div class="alert alert-success" role="alert">На ней есть вода и атмосфера</div>
+                    <div class="alert alert-secondary" role="alert">на ней есть небольшое магнитное поле</div>
+                    <div class="alert alert-warning" role="alert">Наконец она просто красива!</div>
+                  </body>
+                </html>'''
 
 
-@app.route('/promotion')
-def prom():
-    ads = ['Человечество вырастает из детства.', 'Человечеству мала одна планета.',
-           'Мы сделаем обитаемыми безжизненные пока планеты.', 'И начнем с Марса!', 'Присоединяйся!']
-    return '</br>'.join(ads)
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def pep(nickname, level, rating):
+    return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                   <link rel="stylesheet"
+                   href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                   integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                   crossorigin="anonymous">
+                    <title>Результаты</title>
+                  </head>
+                  <body>
+                    <h1>Результаты отбора</h1>
+                    <h2>Претендента на участие в миссии {nickname}<h2>
+                    <div class="alert alert-success" role="alert">Поздравляем! Ваш рейтинг после {level} этапа отбора</div>
+                    <div>составляет {rating}</div>
+                    <div class="alert alert-primary" role="alert">Желаем удачи!</div>
+                  </body>
+                </html>'''
 
 
 @app.route('/promotion_image')
