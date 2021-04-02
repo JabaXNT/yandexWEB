ad206a57a26dacd00bc435f167ba8054cd0d620d
JabaXNT <gluhow2015@gmail.com>
2021-04-02 18:04:53 +0500

    qwertyu

--- FlaskWTF/main.py
+++ FlaskWTF/main.py
@@ -4,14 +4,23 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 @app.route('/')
-@app.route('/index/<title>')
-def index(title):
-    t = title
-    return render_template('base.html', title=t)
+@app.route('/distribution')
+def desc():
+    member_list = ['Ридли скотт', 'Джеймс Кэмерон', 'Шон Бин']
+    return render_template('biba.html', title='По каютам!', mem=member_list)
 
 @app.route('/list_prof/<num>')
 def prof(num):
     return render_template('prof.html', title='/list_prof/<num>', num=num, num2=num)
+
+@app.route('/training/<proff>')
+def training(proff):
+    return render_template('buba.html', title='/training', prof=proff)
+
+@app.route('/index/<title>')
+def index(title):
+    t = title
+    return render_template('base.html', title=t)
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
