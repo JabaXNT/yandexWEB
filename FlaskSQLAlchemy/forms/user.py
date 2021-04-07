e2b932e5b542fd03e5e448001800af80031a3bae
JabaXNT <gluhow2015@gmail.com>
2021-04-07 23:15:20 +0500

    фцыувкаепнрг

--- FlaskSQLAlchemy/forms/user.py
+++ FlaskSQLAlchemy/forms/user.py
@@ -1,13 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, TextAreaField, SubmitField
+from sqlalchemy.sql.sqltypes import Boolean
+from wtforms import PasswordField, StringField, TextAreaField, SubmitField, BooleanField
+from wtforms.fields.core import BooleanField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired
 
 
-class RegisterForm(FlaskForm):
+class LoginForm(FlaskForm):
     email = EmailField('Почта', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
-    password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
-    name = StringField('Имя пользователя', validators=[DataRequired()])
-    about = TextAreaField("Немного о себе")
+    remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
--- FlaskSQLAlchemy/main.py
+++ FlaskSQLAlchemy/main.py
@@ -1,11 +1,20 @@
 from data import db_session
 from data.users import User
 from data.jobs import Jobs
-from forms.user import RegisterForm
-from flask import Flask, render_template, redirect
+from forms.user import LoginForm
+from forms.jobs import JobsForm
+from flask import Flask, render_template, request, make_response, session, redirect
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 
 
 @app.route("/")
@@ -14,9 +23,80 @@ def index():
     jobs = db_sess.query(Jobs).all()
     return render_template("index.html", jobs=jobs)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        print()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+@app.route('/news',  methods=['GET', 'POST'])
+@login_required
+def add_news():
+    form = JobsForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        news = Jobs()
+        news.title = form.title.data
+        news.content = form.content.data
+        news.is_private = form.is_private.data
+        current_user.news.append(news)
+        db_sess.merge(current_user)
+        db_sess.commit()
+        return redirect('/')
+    return render_template('news.html', title='Добавление новости', 
+                           form=form)
+
+@app.route('/news/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit_news(id):
+    form = JobsForm()
+    if request.method == "GET":
+        db_sess = db_session.create_session()
+        news = db_sess.query(Jobs).filter(Jobs.id == id,
+                                          Jobs.user == current_user
+                                          ).first()
+        if news:
+            form.title.data = news.title
+            form.content.data = news.content
+            form.is_private.data = news.is_private
+        else:
+            abort(404)
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        news = db_sess.query(Jobs).filter(Jobs.id == id,
+                                          Jobs.user == current_user
+                                          ).first()
+        if news:
+            news.title = form.title.data
+            news.content = form.content.data
+            news.is_private = form.is_private.data
+            db_sess.commit()
+            return redirect('/')
+        else:
+            abort(404)
+    return render_template('news.html',
+                           title='Редактирование новости',
+                           form=form
+                           )
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
 
 def main():
-    db_session.global_init('db/Space.db')
+    db_session.global_init('db/Mars_Cool.db')
     app.run()
 
 
